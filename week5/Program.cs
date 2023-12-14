6aa0b6ad9c4c2c50587ae4732326d49915f6bae5
Your Name <you@example.com>
2023-12-14 14:28:47 +0300

    week5

---
+++ week5/Program.cs
@@ -0,0 +1,38 @@
+﻿// AND (&&) OR (||) logical operators
+
+//AND
+// true && true => true
+// true && false => false 
+// false && false => false 
+
+string username, password; //admin and admin1234
+
+Console.WriteLine("Enter the username:");
+username = Console.ReadLine();
+
+Console.WriteLine("Enter the password:");
+password = Console.ReadLine();
+
+//if (username == "admin" && password == "admin1234")
+//{
+//    Console.WriteLine("Welcome!");
+//}
+//else
+//{
+//    Console.WriteLine("Invalid user credentials.");
+//}
+
+//OR 
+//true || true => true
+//false || true => true
+//true || false => true
+//false || false => false
+
+if (username != "admin" || password != "admin1234")
+{
+    Console.WriteLine("Invalid user credentials.");
+}
+else
+{
+    Console.WriteLine("Welcome!");
+}
---
+++ week52/Program.cs
@@ -0,0 +1,35 @@
+﻿// 0 (or below) - Freezing
+// from 0 to 5 - Cold
+// from 5 to 10 - Chilly
+// from 10 to 15 - Cool
+// from 15 to 28 - Warm
+// above 28 - Boiling hot
+
+Console.WriteLine("Enter the curent temp:");
+
+int CurrentTemp = int.Parse(Console.ReadLine());
+
+if (CurrentTemp <= 0)
+{
+    Console.WriteLine("Freezing");
+}
+else if (CurrentTemp > 0 && CurrentTemp < 5)
+{
+    Console.WriteLine("Cold");
+}
+else if (CurrentTemp > 5 && CurrentTemp < 10)
+{
+    Console.WriteLine("Chilly");
+}
+else if (CurrentTemp > 10 && CurrentTemp < 15)
+{
+    Console.WriteLine("Cool");
+}
+else if (CurrentTemp > 15 && CurrentTemp < 28)
+{
+    Console.WriteLine("Warm");
+}
+else
+{
+    Console.WriteLine("Boiling hot");
+}
